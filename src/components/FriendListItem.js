fe982f304c295b6059b6829ebaf8d7e24dcc79bb
rkukin <romakukin@gmail.com>
2020-01-21 22:11:59 +0200

    working on styling

--- src/components/FriendListItem.js
+++ src/components/FriendListItem.js
@@ -1,21 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from "styled-components";
 
 
 function FriendListItem({ friend: { avatar, name, isOnline, id } }) {
 
-    let onlineStatus = ["isOnline"];
-    if (isOnline){
-        onlineStatus.push("online")
-    } else {
-        onlineStatus.push("offline")
-    }
+    const ItemContainer = styled.li`
+      display: flex;
+      max-width: 300px;
+      box-shadow: 0 2px 2px rgba(2, 2, 2, 0.2);
+      padding: 10px;
+      margin-bottom: 15px;
+      &:last-child {
+        margin-bottom: 100px;
+      }
+    `;
+
+    const Status = styled.span`
+      background-color: ${isOnline ? 'green' : 'red'};
+      border-radius: 50%;
+      width: 20px;
+      height: 20px;
+`;
+    const Avatar = styled.img`
+      border-radius: 10px;
+`;
+
+
   return (
-    <li key={id} className="item">
-      <span className={onlineStatus.join(" ")}/>
-      <img className="avatar" src={avatar} alt="avatar" />
+    <ItemContainer key={id} className="item">
+      <Status className="isOnline"/>
+      <Avatar className="avatar" src={avatar} alt="avatar" />
       <p className="name">{name}</p>
-    </li >
+    </ItemContainer >
   )
 }
 
--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -1,20 +1,80 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from "styled-components";
 
 function Statistics({ title, stats }) {
-  return (
-    <section className="statistics">
-      {title && <h2 className="title">{title}</h2>}
 
-      {stats.length > 0 && <ul className="stat-list">
+  const STATBGCOLORS = [
+
+  ]
+
+  const Statistics = styled.section`
+    max-width: 268px;
+    box-shadow: 0 2px 2px rgba(2, 2, 2, 0.02);
+    background-color: #ffffff;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    margin-bottom: 100px;
+  `;
+
+  const Title = styled.h2`
+    color: #475965;
+    font-family: "Proxima Nova - Semibold";
+    font-size: 16px;
+    font-weight: 400;
+    text-transform: uppercase;
+    letter-spacing: 0.16px;
+    min-height: 70px;
+    line-height: 70px;
+    display: block;
+  `;
+
+  const StatList = styled.ul`
+display: flex;
+flex-direction: row;
+min-width: 100%;
+`;
+
+  const StatListItem = styled.li`
+display: flex;
+flex-direction: column;
+justify-content: space-around;
+min-width: 20%;
+align-items: center;
+min-height: 60px;
+`;
+
+  const StatLabel = styled.span`
+font-family: "Proxima Nova";
+font-size: 12px;
+font-weight: 300;
+line-height: 20px;
+letter-spacing: 0.3px;
+`;
+
+  const StatPercantage = styled.span`
+    font-family: "Proxima Nova";
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 20px;
+    letter-spacing: 0.45px;
+`;
+
+
+  return (
+    <Statistics className="statistics">
+      {title && <Title className="title">{title}</Title>}
+      {stats.length > 0 && <StatList className="stat-list">
         {stats.map(stat => (
-          <li key={stat.id}>
-            <span>{stat.label}</span>
-            <span>{stat.percentage}</span>
-          </li>
+          <StatListItem key={stat.id}>
+            <StatLabel>{stat.label}</StatLabel>
+            <StatPercantage>{stat.percentage}</StatPercantage>
+          </StatListItem>
         ))}
-      </ul>}
-    </section>)
+      </StatList>}
+    </Statistics>)
 }
 
 Statistics.propTypes = {
