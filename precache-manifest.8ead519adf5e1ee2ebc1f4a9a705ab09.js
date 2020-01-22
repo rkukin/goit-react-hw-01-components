9349ec35bef2091cbc896bffe1b81b93e6a02e20
rkukin <romakukin@gmail.com>
2020-01-22 09:04:02 +0200

    Updates

---
+++ precache-manifest.8ead519adf5e1ee2ebc1f4a9a705ab09.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c130e12d375236d5120044a68c25e4f",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "c93113bd82e2985402a7",
+    "url": "/goit-react-hw-01-components/static/js/2.d87c9d7f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/goit-react-hw-01-components/static/js/2.d87c9d7f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f25e18a954f6a3b3e555",
+    "url": "/goit-react-hw-01-components/static/js/main.7caedc9e.chunk.js"
+  },
+  {
+    "revision": "5529903743abc23e6f33",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.5c5371af.js"
+  }
+]);
